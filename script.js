5b8782b4b50d52a5efef12ab3dc80a0dbc841587
Bahaa Mohammed <45239532+bahaamohammed@users.noreply.github.com>
2024-10-09 22:01:59 +0300

    Update js and finish the project

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelector(".input-box");
 
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes-app");
+}
+showNotes();
+
+function updateStorage(){
+    localStorage.setItem("notes-app", notesContainer.innerHTML);
+}
+
 createBtn.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -14,5 +23,14 @@ createBtn.addEventListener("click", ()=>{
 notesContainer.addEventListener("click", function(e){
     if(e.target.tagName === "IMG"){
         e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        });
     }
 });
